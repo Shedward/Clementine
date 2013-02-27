2aa301ada04ee689267b5c35f52413bc6bab37df
Maltsev Vlad <shedwardx@gmail.com>
2013-02-27 10:46:52 +0700

    Connect VkService with application. Appear vk root item in 'internet' panel.

--- src/internet/vkservice.cpp
+++ src/internet/vkservice.cpp
@@ -1,6 +1,24 @@
 #include "vkservice.h"
 
-VkService::VkService(QObject *parent) :
-    InternetService(parent)
+const char* VkService::kServiseName = "Vk.com";
+
+VkService::VkService(Application *app, InternetModel *parent) :
+    InternetService(kServiseName, app, parent, parent)
+{
+}
+
+VkService::~VkService()
+{
+}
+
+
+QStandardItem *VkService::CreateRootItem()
+{
+    root_item_ = new QStandardItem(QIcon(":providers/vk.ico"),kServiseName);
+   // root_item_->setData(true, );
+    return root_item_;
+}
+
+void VkService::LazyPopulate(QStandardItem *parent)
 {
 }
--- src/internet/vkservice.h
+++ src/internet/vkservice.h
@@ -2,17 +2,40 @@
 #define VKSERVICE_H
 
 #include "internetservice.h"
+#include "internetmodel.h"
+#include "core/song.h"
+
 
 class VkService : public InternetService
 {
     Q_OBJECT
 public:
-    explicit VkService(QObject *parent = 0);
+    explicit VkService(Application* app, InternetModel* parent);
+    ~VkService();
+
+    static const char* kServiseName;
+    static const char* kSettingGroup;
+    static const char* kApiKey;
+
+    enum ItemType {
+        Type_Root = InternetModel::TypeCount,
+        Type_Recommendations,
+        Type_MyMusic,
+        Type_Popular,
+        Type_Friends,
+        Type_Groups
+
+    };
+
+    QStandardItem* CreateRootItem();
+    void LazyPopulate(QStandardItem *parent);
     
 signals:
     
 public slots:
-    
+
+private:
+    QStandardItem* root_item_;
 };
 
 #endif // VKSERVICE_H
