3eadd776cfd3e2a3c0817068062fef6f7066dbda
Maltsev Vlad <shedwardx@gmail.com>
2013-04-09 21:07:23 +0700

    Cashing settings

--- src/internet/vksettingspage.cpp
+++ src/internet/vksettingspage.cpp
@@ -1,5 +1,8 @@
 #include "vksettingspage.h"
 
+#include <QDir>
+#include <QFileDialog>
+
 #include "ui_vksettingspage.h"
 #include "core/application.h"
 #include "core/logging.h"
@@ -14,6 +17,10 @@ VkSettingsPage::VkSettingsPage(SettingsDialog *parent)
     ui_->setupUi(this);
     connect(service_, SIGNAL(LoginSuccess(bool)),
             SLOT(LoginSuccess(bool)));
+    connect(ui_->choose_path, SIGNAL(clicked()),
+            SLOT(CasheDirBrowse()));
+    connect(ui_->reset, SIGNAL(clicked()),
+            SLOT(ResetCasheFilenames()));
 }
 
 VkSettingsPage::~VkSettingsPage()
@@ -28,6 +35,12 @@ void VkSettingsPage::Load()
 
     ui_->maxGlobalSearch->setValue(s.value("maxSearchResult",100).toInt());
 
+    bool enable_cashing = s.value("enable_cashing", false).toBool();
+    ui_->enable_cashing->setChecked(enable_cashing);
+    QString def_path = QDir::toNativeSeparators(QDir::homePath() + "/Vk Cashe");
+    ui_->cashe_dir->setText(s.value("cashe_path",def_path).toString());
+    ui_->cashe_filename->setText(s.value("cashe_filename","%artist - %title").toString());
+
     if (service_->HasAccount()) {
         Login();
     } else {
@@ -41,6 +54,9 @@ void VkSettingsPage::Save()
     s.beginGroup(VkService::kSettingGroup);
 
     s.setValue("maxSearchResult",ui_->maxGlobalSearch->value());
+    s.setValue("enable_cashing",ui_->enable_cashing->isChecked());
+    s.setValue("cashe_path",ui_->cashe_dir->text());
+    s.setValue("cashe_filename", ui_->cashe_filename->text());
 }
 
 void VkSettingsPage::Login()
@@ -81,5 +97,20 @@ void VkSettingsPage::Logout()
     disconnect(ui_->login_button, SIGNAL(clicked()),
                this, SLOT(Logout()));
     ui_->account->setEnabled(true);
+}
+
+void VkSettingsPage::CasheDirBrowse()
+{
+    QString directory = QFileDialog::getExistingDirectory(
+          this, tr("Choose Vk.com cashe directory"), ui_->cashe_dir->text());
+    if (directory.isEmpty())
+      return;
+
+    ui_->cashe_dir->setText(QDir::toNativeSeparators(directory));
+}
+
+void VkSettingsPage::ResetCasheFilenames()
+{
+    ui_->cashe_filename->setText("%artist - %title");
 }
 
--- src/internet/vksettingspage.h
+++ src/internet/vksettingspage.h
@@ -26,6 +26,9 @@ private slots:
     void Login();
     void Logout();
 
+    void CasheDirBrowse();
+    void ResetCasheFilenames();
+
 private:
     Ui_VkSettingsPage* ui_;
     VkService* service_;
