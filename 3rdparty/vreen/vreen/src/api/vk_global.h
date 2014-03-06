7fffafb99d7d58a0d331e512c260fcb0b9e8c74c
Maltsev Vlad <shedwardx@gmail.com>
2014-03-07 01:11:48 +0700

    Fix dllimport warnings for windows build.

--- 3rdparty/vreen/vreen/src/api/vk_global.h
+++ 3rdparty/vreen/vreen/src/api/vk_global.h
@@ -27,12 +27,7 @@
 
 #include <QObject>
 
-#if defined(VK_LIBRARY)
-#  define VK_SHARED_EXPORT Q_DECL_EXPORT
-#else
-#  define VK_SHARED_EXPORT Q_DECL_IMPORT
-#endif
-
+#define VK_SHARED_EXPORT
 typedef QList<int> IdList;
 
 #endif // API_GLOBAL_H
